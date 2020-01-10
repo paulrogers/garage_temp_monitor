9dbf21b84f5ea9759dd969b923968decb0e9741d
Paul Rogers <paul.rogers@shaw.ca>
2020-01-09 20:51:03 -0700

    more features

--- combined.ino/combined.ino.ino
+++ combined.ino/combined.ino.ino
@@ -43,22 +43,29 @@ Adafruit_LiquidCrystal lcd(0);
 
 
 // Values for thermostat control
-
+// the default set temp when we start up
 float default_temp = 18.00;
+
+// the value where we ignore temp changes after reaching our set point
 float  default_swing = 2.0;
 
-//   the heater on/off button 
-bool heater_on = false;
+//   the heater on/off button
+bool heater_enabled = false;
 
 // is the heater /actually/ on
 bool heating = false;
 
-const int heaterOnOffPin = 8;
-const int heatUpPin = 9;
-const int heatDownPin = 10;
+// pins for the buttons
+const int heaterOnOffPin = 9;
+const int heatUpPin = 10;
+const int heatDownPin = 8;
+
+// the relay pin
 const int heaterRelayPin = 11;
 
+// the temperature we want to aim for
 float setTemperature;
+// the increment in temperature between button presses
 float tempIncrement = 0.5;
 
 
@@ -66,30 +73,64 @@ float tempIncrement = 0.5;
 Average<float> ave(20);
 
 
-// avoid using the delay)( function so we get fast feedback on the switches
-unsigned long previousMillis = 0;        // will store last time LED was updated
+// avoid using the delay() function so we get fast feedback on the switches
+unsigned long previousMillis = 0;        // will store last time loop was executed
+const long sensorReadInterval = 5000;
 
-// constants won't change:
-const long interval = 5000;         
+bool heaterOnOffButtonState_LAST = true;
+bool heaterUpButtonState_LAST = true;
+bool heaterDownButtonState_LAST = true;
+
+float t1;
+float h1;
+float t2;
+float h2;
 
 
-void setHeaterState( bool onOrOff){
+// constant for interval before allowing heater to come on again
+const  long five_minutes =  300000 ;
+unsigned long lastHeaterStoppedAt = 0 ;
 
-  if (onOrOff){
+
+void setHeaterState( bool onOrOff) {
+
+  if (onOrOff) {
     heating = true;
-    digitalWrite( heaterRelayPin , HIGH);
+    digitalWrite( heaterRelayPin , LOW);
+    writeHeaterStateToLCD(" on");
   } else {
-    heating = true;
-    digitalWrite( heaterRelayPin, LOW );
+    heating = false;
+    digitalWrite( heaterRelayPin, HIGH );
+    writeHeaterStateToLCD(" off");
   }
 }
 
 
-void writeSetTempToLCD( float setTemp){
-  lcd.setCursor(5,3);
-  lcd.write( setTemp);
-  
+void writeSetTempToLCD( float setTemp) {
+  lcd.setCursor(5, 3);
+  lcd.print( "     " );
+  lcd.setCursor(5, 3);
+  lcd.print( setTemp);
+
 }
+
+void writeHeaterEnabledToLCD( bool heatEnabled) {
+  lcd.setCursor(11, 3);
+  lcd.print( "     " );
+  lcd.setCursor(11, 3);
+  if (heatEnabled)
+    lcd.print( " ON");
+  else
+    lcd.print( " OFF");
+}
+
+void writeHeaterStateToLCD(String wait) {
+  lcd.setCursor(15, 3);
+  while (wait.length() < 4 )
+    wait = wait + " ";
+  lcd.print( wait );
+}
+
 
 void setup() {
   Serial.begin(9600);
@@ -102,94 +143,123 @@ void setup() {
   pinMode(heaterOnOffPin, INPUT_PULLUP);
   pinMode(heatUpPin, INPUT_PULLUP);
   pinMode(heatDownPin, INPUT_PULLUP);
-  
-  
+
+
   pinMode(heaterRelayPin, OUTPUT);
-  digitalWrite( heaterRelayPin, LOW);
+  digitalWrite( heaterRelayPin, HIGH);
   setTemperature = default_temp;
 }
 
+
 void loop() {
-  // Wait a few seconds between measurements.
-  //delay(2000);
 
 
-   // set the heater on or off
-   /*
-    *  if HEATER_OFF   - off
-    *  if HEATER_ON and set_temp > t1  OFF
-    *  if HEATER_ON and (set_temp+ ( -1*default_swing) ) <= t1
-    *  
-    *  we also want to average the last XX readings beore deciding...phase 2
-    *  
-    */
 
-    // read state of on/off button
-    bool heaterOnOffButtonState = digitalRead(heaterOnOffPin);
-    if (!heaterOnOffButtonState ){
-      heater_on  = !heater_on;
-      
-      lcd.setCursor(17,3);
-      lcd.print(heaterOnOffButtonState);
-      
+  // read state of on/off button
+  bool heaterOnOffButtonState = digitalRead(heaterOnOffPin);
+  if (!heaterOnOffButtonState ) {
+    if ( heaterOnOffButtonState_LAST ) {
+      heater_enabled  = !heater_enabled;
+      Serial.println("on/off button pressed");
+      writeHeaterEnabledToLCD(heater_enabled);
     }
+  }
+  heaterOnOffButtonState_LAST = heaterOnOffButtonState;
+  delay(50);
 
-    bool heaterUpButtonPressed = digitalRead(heatUpPin);
-    if ( !heaterUpButtonPressed){
-      lcd.setCursor(18,3);
-      lcd.print(heaterUpButtonPressed);
+  bool heaterUpButtonPressed = digitalRead(heatUpPin);
+  if ( !heaterUpButtonPressed) {
+    if ( heaterOnOffButtonState_LAST ) {
       setTemperature = setTemperature + tempIncrement;
       writeSetTempToLCD( setTemperature );
+      lcd.print(heaterUpButtonPressed);
+      Serial.println("up button pressed");
     }
+  }
+  heaterOnOffButtonState_LAST = heaterUpButtonPressed;
+  delay(50);
 
-    bool heaterDownButtonPressed = digitalRead(heatDownPin);
-    if ( !heaterDownButtonPressed){
-      lcd.setCursor(19,3);
-      lcd.print(heaterDownButtonPressed);
+  bool heaterDownButtonPressed = digitalRead(heatDownPin);
+  if ( !heaterDownButtonPressed) {
+    if ( heaterDownButtonState_LAST ) {
       setTemperature = setTemperature - tempIncrement;
       writeSetTempToLCD( setTemperature );
+      Serial.println("down button pressed");
     }
-    if ( heaterOnOffButtonState && !heater_on  ){
-      
+  }
+  heaterDownButtonState_LAST = heaterUpButtonPressed;
+  delay(50);
+
+  if ( heater_enabled  ) {
+
+    if ( (setTemperature > ( ave.mean() ))  && !heating  ) {
+      Serial.println("set temp is more than actual, enabling heater, if it hasnt ben used recently");
+      // dont start heater within 5 minuts of last heat cycle
+      unsigned long currentHeaterMillis = millis();
+      Serial.println( currentHeaterMillis );
+      Serial.println( five_minutes );
+      Serial.println( lastHeaterStoppedAt );
+
+      // if we cycle the heater, with in 5 minutes, we get a -ve, which is > five_minutes
+      // so we need to check for > 0 too, but then we get cast issues....
+      // As long as we dont cycle in the first 5 minutes, I think its all ok....
+      Serial.println(currentHeaterMillis - five_minutes );
+      if ( (currentHeaterMillis - five_minutes)  > 0 ) {
+        if ( ( currentHeaterMillis - five_minutes ) > lastHeaterStoppedAt  ) {
+          setHeaterState( true );
+        }  else {
+          Serial.println("Want to heat, but need to wait..");
+          writeHeaterStateToLCD("wait");
+        }
+      } else {
+        Serial.println("Cant Heat cycle in first 5 minutes. Need to wait..");
+        writeHeaterStateToLCD("init");
+      }
     }
-      
-  
+  } else {
+      // likley the on/off button has been set to off
+      if ( heating) {
+        setHeaterState( false );
+        lastHeaterStoppedAt = millis();
+        Serial.println(" heater set to off ");
+      }    
+  }
+  if ( (setTemperature <= ( ave.mean()   ) ) && heating) {
+    Serial.println("Desired Temperature reached. Heater being switched off!");
+    setHeaterState( false );
+    lastHeaterStoppedAt = millis();
+  }
 
-    
-
-
-    // read higher/lower buttons
 
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= sensorReadInterval) {
     previousMillis = currentMillis;
-  
+
     // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float h1 = dht1.readHumidity();
-    // Read temperature as Celsius (the default)
-    float t1 = dht1.readTemperature();
-    float h2 = dht2.readHumidity();
-    // Read temperature as Celsius (the default)
-    float t2 = dht2.readTemperature();
-  
+
+    h1 = dht1.readHumidity();
+    t1 = dht1.readTemperature();
+
+    h2 = dht2.readHumidity();
+    t2 = dht2.readTemperature();
+
 
     // record the temp to get our rolling average
     ave.push( t1 );
-  
-  
-  
-    
+
+
+
+
     // Check if any reads failed and exit early (to try again).
     /*
-    if (isnan(h1) || isnan(t1) ) {
+      if (isnan(h1) || isnan(t1) ) {
       //Serial.println(F("Failed to read from DHT sensor!"));
       //return;
       h1  ="?";
       t1 = "?";
-    }
+      }
     */
-  
+
     Serial.print(F("Humidity: "));
     Serial.print(h1);
     Serial.print(F("%  Temperature: "));
@@ -199,35 +269,32 @@ void loop() {
     Serial.print(" set: ");
     Serial.print( setTemperature );
     Serial.print(" enabled: ");
-    Serial.print( heater_on );
-    
+    Serial.print( heater_enabled );
+
     Serial.print(" heating: ");
     Serial.println( heating );
-    
-    
+
+
     lcd.setCursor(0, 0);
     lcd.print("In:  ");
     lcd.print( t1 );
     lcd.print(" ");
     lcd.print(h1);
-  
+
     lcd.setCursor(0, 1);
     lcd.print("Out: ");
     lcd.print( t2 );
     lcd.print(" ");
     lcd.print(h2);
-  
-    // verages, for debug
-    lcd.setCursor(0,2);
-    lcd.print("av:");
+
+    // averages, for debug
+    lcd.setCursor(0, 2);
+    lcd.print("av: ");
     lcd.print(ave.mean() );
-  
-    lcd.setCursor(0,3);
+
+    lcd.setCursor(0, 3);
     lcd.print("Set: ");
-    lcd.print( setTemperature);
-    if (heater_on)
-      lcd.print(" ON ");
-    else
-      lcd.print(" OFF");  
-  }    
+    writeSetTempToLCD( setTemperature );
+    writeHeaterEnabledToLCD( heater_enabled);
+  }
 }
